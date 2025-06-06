73dde17d1e1518edb50c60cf2509679bc9fb7c31
silvioprogc <silviosramos@hotmail.com>
2025-06-06 17:03:51 +0000

    desavio aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,33 +1,51 @@
 #include <stdio.h>
 
 int main() {
-    
+    // Número de casas para movimentar cada peça
     int casasTorre = 5;
     int casasBispo = 5;
     int casasRainha = 8;
 
-    printf("Movimento da Torre:\n");
-    // Torre: move-se em linha reta (horizontal ou vertical)
+    //Torre
+    printf("Movimento da Torre (usando for):\n");
     for (int i = 1; i <= casasTorre; i++) {
         printf("Direita\n");
     }
 
-    printf("\nMovimento do Bispo:\n");
-    // Bispo move-se na diagonal
-  
+    //Bispo 
+    printf("\nMovimento do Bispo (usando while):\n");
     int i = 1;
     while (i <= casasBispo) {
         printf("Cima Direita\n");
         i++;
     }
 
-    printf("\nMovimento da Rainha:\n");
-    // Rainha 8 casas para a esquerda
+    //Rainha
+    printf("\nMovimento da Rainha (usando do-while):\n");
     int j = 1;
     do {
         printf("Esquerda\n");
         j++;
     } while (j <= casasRainha);
+
+    //Cavalo
+    printf("\nMovimento do Cavalo:\n");
+
+    int passosBaixo = 2;
+    int passosEsquerda = 1;
+
+    for (int movimento = 1; movimento <= 1; movimento++) {
+               int passo = 1;
+        while (passo <= passosBaixo) {
+            printf("Baixo\n");
+            passo++;
+        }        
+        int k = 0;
+        while (k < passosEsquerda) {
+            printf("Esquerda\n");
+            k++;
+        }
+    }
 
     return 0;
 }
