40d304139ba4373ed1e63f9385c93e12703b7a8d
silvioprogc <silviosramos@hotmail.com>
2025-06-07 17:09:57 +0000

    desafio xadrez mestre

--- xadrez.c
+++ xadrez.c
@@ -1,51 +1,74 @@
 #include <stdio.h>
 
+//Torre
+void moverTorre(int casasRestantes) {
+    if (casasRestantes == 0)
+        return;
+    printf("Direita\n");
+    moverTorre(casasRestantes - 1);
+}
+
+// Bispo
+void moverBispoRecursivo(int casasRestantes) {
+    if (casasRestantes == 0)
+        return;
+    printf("Cima Direita\n");
+    moverBispoRecursivo(casasRestantes - 1);
+}
+
+// Rainha
+void moverRainha(int casasRestantes) {
+    if (casasRestantes == 0)
+        return;
+    printf("Esquerda\n");
+    moverRainha(casasRestantes - 1);
+}
+
 int main() {
-    // Número de casas para movimentar cada peça
+    // Números de casas
     int casasTorre = 5;
     int casasBispo = 5;
     int casasRainha = 8;
 
-    //Torre
-    printf("Movimento da Torre (usando for):\n");
-    for (int i = 1; i <= casasTorre; i++) {
-        printf("Direita\n");
-    }
+    //Torre (recursiva)
+    printf("Movimento da Torre:\n");
+    moverTorre(casasTorre);
 
-    //Bispo 
-    printf("\nMovimento do Bispo (usando while):\n");
-    int i = 1;
-    while (i <= casasBispo) {
-        printf("Cima Direita\n");
-        i++;
+    //Bispo
+    printf("\nMovimento do Bispo:\n");
+    
+    moverBispoRecursivo(casasBispo);
+    
+    printf("\nBispo:\n");
+    int movimentosVerticais = 3;
+    int movimentosHorizontais = 3;
+    for (int v = 0; v < movimentosVerticais; v++) {
+        for (int h = 0; h < movimentosHorizontais; h++) {
+            printf("Cima Direita\n");
+        }
     }
 
     //Rainha
-    printf("\nMovimento da Rainha (usando do-while):\n");
-    int j = 1;
-    do {
-        printf("Esquerda\n");
-        j++;
-    } while (j <= casasRainha);
+    printf("\nMovimento da Rainha:\n");
+    moverRainha(casasRainha);
 
     //Cavalo
     printf("\nMovimento do Cavalo:\n");
+    int movimentosCavalo = 2; // número de "L"s
+    int maxAltura = 2;
+    int maxLargura = 1;
 
-    int passosBaixo = 2;
-    int passosEsquerda = 1;
-
-    for (int movimento = 1; movimento <= 1; movimento++) {
-               int passo = 1;
-        while (passo <= passosBaixo) {
-            printf("Baixo\n");
-            passo++;
-        }        
-        int k = 0;
-        while (k < passosEsquerda) {
-            printf("Esquerda\n");
-            k++;
+    for (int l = 0; l < movimentosCavalo; l++) {
+        for (int cima = 0, direita = 0; cima <= maxAltura || direita <= maxLargura; cima++, direita++) {
+            if (cima < maxAltura) {
+                printf("Cima\n");
+            } else if (direita < maxLargura) {
+                printf("Direita\n");
+            }
+    
         }
     }
 
     return 0;
+
 }
